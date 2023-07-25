a1c371d08158911334f9c53a7c0ce2b3523af615
Samora Laurent <laurentsamora6@gmail.com>
2023-07-25 18:32:25 +0300

    created the task layer

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,29 +1,41 @@
 /* eslint-disable react/prop-types */
 
 import useTask from "../../hooks/useTask";
-import { Stack, Text } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import SkeletonLoader from "../SkeletonLoader/SkeletonLoader";
 import TaskPost from "../TaskPost/TaskPost";
 
-const AboutMe = ({ dyUser, id }) => {
+const AboutMe = ({ dyUser }) => {
+  const id = dyUser?._id;
   const { tasks } = useTask();
-
-  const getTasks = tasks.filter((task) => task?.userId === id);
 
   if (!tasks) {
     return <SkeletonLoader />;
   }
 
+  const getTasks = tasks.filter((task) => task?.userId === id);
+
   return (
     <Stack>
       <Text>About Me</Text>
       <Text>{dyUser?.desc}</Text>
-      <Stack w="80%" gap={20} pt={10} m="auto">
+      <Stack w="90%" gap={5} pt={10} m="auto">
         {getTasks.length === 0 && (
           <Text textAlign="center" color="gray.500">
             There are no tasks yet
           </Text>
         )}
+        <Box
+          w="100%"
+          h="10"
+          border="1px solid"
+          borderRadius="md"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text>Activities</Text>
+        </Box>
         {getTasks &&
           getTasks.map((task) => <TaskPost key={task?._id} task={task} />)}
       </Stack>
--- src/components/TaskPost/TaskPost.jsx
+++ src/components/TaskPost/TaskPost.jsx
@@ -1,7 +1,63 @@
-const TaskPost = ({ task }) => {
-  console.log(task?.task);
+/* eslint-disable react/prop-types */
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Progress,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
-  return <div>TaskPost</div>;
+const TaskPost = ({ task }) => {
+  return (
+    <Stack
+      w="100%"
+      gap={4}
+      borderRadius="md"
+      display="flex"
+      justifyContent="center"
+    >
+      <Text>Activity: {task?.task}</Text>
+      <Flex justify="space-between">
+        <Text>Achieved: {task.achieved}</Text>
+      </Flex>
+
+      <Box>
+        <Text>
+          Status:{" "}
+          {task.status === "completed"
+            ? "Completed"
+            : task.status === "in-progress"
+            ? "In Progress"
+            : "Not Started"}
+        </Text>
+      </Box>
+      <Box>
+        <Text>
+          Created on:{" "}
+          {task?.dateTime.split("T")[0].split("-").reverse().join("-")}
+        </Text>
+      </Box>
+      <Progress
+        colorScheme="green"
+        size="lg"
+        value={(task.achieved / task.target) * 100}
+      />
+      <Flex justify="space-between">
+        <Input w="250px" placeholder="Enter achieved:" />
+        <Button
+          bg="teal"
+          variant="unstyled"
+          color="white"
+          px={4}
+          // onClick={handleLogEvent}
+        >
+          Log Event
+        </Button>
+      </Flex>
+    </Stack>
+  );
 };
 
 export default TaskPost;
